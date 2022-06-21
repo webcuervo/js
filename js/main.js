79171b25120c6c25131912a336274b489b3df861
Esteban Cuervo <webcuervo@gmail.com>
2022-06-21 15:46:00 -0300

    prueba js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt("ingresa tu edad"))
+let nombre = prompt("ingresa tu nombre")
+let apellido = prompt("ingresa tu apellido")
+
+let salida = "Nombre y apellido: " + nombre + " " + apellido + ", de " + edad + " años" 
+
+alert(salida)

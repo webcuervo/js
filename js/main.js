5a6a7efa6b87edf257177eba9843779213f567d0
Esteban Cuervo <webcuervo@gmail.com>
2022-07-05 18:32:23 -0300

    arreglo del for

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 let cantidad = parseInt(prompt("Ingrese un numero"))
 
-
-for (let i = 1 ; i = cantidad ; i++) {
+for (let i = 1 ; i <= cantidad ; i++) {
 
     if(i%2===0 && i!=0) {
-        document.write(i + "es par <br>")
+        document.write(i + "<span> es par </span><br>")
     }
 
-    else {(i%2===0 && i!=0) 
-        document.write(i + "es impar <br>")
+    else {
+        document.write(i + "<span> es impar </span><br>")
     }
 }
-
